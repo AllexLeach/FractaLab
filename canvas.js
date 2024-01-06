8978714c93250a7d41043ee8dd0b2e58299e7d8b
AllexLeach <allexleach@gmail.com>
2024-01-06 22:22:59 +0300

    button_syka_blayt!!

--- canvas.js
+++ canvas.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext("2d");
 var width = canvas.width = 750 /*window.innerWidth;*/
 var height = canvas.height = 750 /*window.innerHeight;*/
 
-var branchAng = document.getElementById("branchAngle");
+var branchAng = document.getElementById("input_range_branchAngle");
 var numBranch = document.getElementById("numberBranches");
 var ang = document.getElementById("angle3");
 var branchLen = document.getElementById("lengthBranch");
@@ -14,52 +14,73 @@ var i = document.getElementById('color');
 
 const color = ["#000", "red", "#f90", "#EAF900", "#0fb000", "#0ef", "#20f", "#b0f"];
 
-document.getElementById("branchAngleButton").onclick = function() {
-  var numberBranches = numBranch.value;
+let branchAngleValue = 6;
+let angValue = 3;
+let changeBranchAngleValue = false;
 
-  var angle3 = ang.value
-  var branchAngle = Math.PI / branchAng.value; // anything degrees
-  var branchLength = height / branchLen.value; 
+branchAng.addEventListener('input', function() {
+   branchAngleValue = branchAng.value;
+   changeBranchAngleValue = true;
+});
+
+ang.addEventListener('input', () => {
+   angValue = ang.value;
+   changeBranchAngleValue = false;
+});
+
+branchAng.addEventListener('change', () => {
+   if (changeBranchAngleValue == true) {
+      angValue = branchAngleValue/2;
+      ang.value = branchAngleValue/2;  
+   };
+});
+
+document.getElementById("branchAngleButton").onclick = function() {
+   var numberBranches = numBranch.value;
+
+   var angle3 = angValue;
+   var branchAngle = Math.PI / branchAngleValue; // anything degrees
+   var branchLength = height / branchLen.value; 
 
 //   console.log(color[i.value]);
-//   console.log(i.value);
+   console.log(branchAng.value);
       
-  function drawBranch(x, y, length, angle, depth) {
-    if (depth > 0) {
-      var x2 = x + Math.cos(angle) * length;
-      var y2 = y + Math.sin(angle) * length;
-        
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.lineTo(x2, y2);
-      ctx.strokeStyle = color[i.value];
-      ctx.lineWidth = depth * 1;
-      ctx.stroke();
-        
-      if (numberBranches == 2) {
-        drawBranch(x2, y2, length * 0.7, angle + branchAngle, depth - 1);
-        drawBranch(x2, y2, length * 0.7, angle - branchAngle, depth - 1);
-      } else if (numberBranches == 3) {
-        drawBranch(x2, y2, length * 0.7, angle + branchAngle, depth - 1);
-        drawBranch(x2, y2, length * 0.7, angle - branchAngle, depth - 1);
-        drawBranch(x2, y2, length * 0.3, -angle3 * branchAngle, depth - 1);
+   function drawBranch(x, y, length, angle, depth) {
+      if (depth > 0) {
+         var x2 = x + Math.cos(angle) * length;
+         var y2 = y + Math.sin(angle) * length;
+         
+         ctx.beginPath();
+         ctx.moveTo(x, y);
+         ctx.lineTo(x2, y2);
+         ctx.strokeStyle = color[i.value];
+         ctx.lineWidth = depth * 1;
+         ctx.stroke();
+         
+         if (numberBranches == 2) {
+         drawBranch(x2, y2, length * 0.7, angle + branchAngle, depth - 1);
+         drawBranch(x2, y2, length * 0.7, angle - branchAngle, depth - 1);
+         } else if (numberBranches == 3) {
+         drawBranch(x2, y2, length * 0.7, angle + branchAngle, depth - 1);
+         drawBranch(x2, y2, length * 0.7, angle - branchAngle, depth - 1);
+         drawBranch(x2, y2, length * 0.3, -angle3 * branchAngle, depth - 1);
+         }
       }
-    }
-  }
+   }
 
       
-  function animate() {
-    angle += 0.01; 
-    ctx.clearRect(0, 0, width, height);
-    drawBranch(width / 2, height - 50, branchLength, -Math.PI / 2, 9);
-    requestAnimationFrame(animate);
-  }
+   function animate() {
+      angle += 0.01; 
+      ctx.clearRect(0, 0, width, height);
+      drawBranch(width / 2, height - 50, branchLength, -Math.PI / 2, 9);
+      requestAnimationFrame(animate);
+   }
       
-  animate();
+   animate();
 }
 
 document.getElementById("reload").onclick = function() {
-  location.reload();
+   location.reload();
 }
 
 function drawBranchFirst(x, y, length, angle, depth) {
